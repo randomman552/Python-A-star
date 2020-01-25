ec4c7e1a2f019dbea013090533ed6a76374f1ba5
George Grainger <GraingerGY@cardiff.ac.uk>
2020-01-25 23:33:42 +0000

    Inital tile system implemented (need to OOP)

--- pathfinding_AStar.py
+++ pathfinding_AStar.py
@@ -86,28 +86,68 @@ class tile(object):
         self.size = size
         self.x_pos = x_pos
         self.y_pos = y_pos
+        self.color = (255,255,255)
 
     def draw(self):
         if self.enabled:
-            pygame.draw.rect(self.window, (255,255,255), (self.x_pos, self.y_pos, self.size, self.size))
+            pygame.draw.rect(self.window, self.color, (self.x_pos + 1, self.y_pos + 1, self.size - 2, self.size - 2))
 
 StartWindow = DefineSettings()
 Settings = StartWindow.open()
 
 pygame.init()
-windowSize = (Settings["grid size"]["x"] * Settings["tile size"], Settings["grid size"]["y"] * Settings["tile size"])
+borderSize = 10
+windowSize = (Settings["grid size"]["x"] * Settings["tile size"] + borderSize, Settings["grid size"]["y"] * Settings["tile size"] + borderSize)
 window = pygame.display.set_mode(windowSize)
 pygame.display.set_caption("A* Path Finder")
 
+tile_list = []
+for x in range(Settings["grid size"]["x"]):
+    for y in range(Settings["grid size"]["y"]):
+        tileSize = Settings["tile size"]
+        tile_list.append(tile(window, tileSize, borderSize // 2 + x * tileSize, borderSize // 2 + y * tileSize))
+
+
+def get_cur_tile(pos):
+    x_num = Settings["grid size"]["x"]
+    y_num = Settings["grid size"]["y"]
+    tile_x = int((pos[1] / tileSize) + 1)
+    tile_y = int((pos[0] / tileSize) + 1)
+    if tile_x > x_num or tile_y > y_num:
+        return None
+    tile = tile_list[((tile_y - 1) * x_num) + tile_x - 1]
+    tile_pos = [tile_x, tile_y]
+    return tile
 
 running = True
+mouseDown = False
 while running:
-    pygame.time.delay(100)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            mouseDown = True 
+        elif event.type == pygame.MOUSEBUTTONUP:
+            mouseDown = False
+    
+    if mouseDown:
+        mousePresses = pygame.mouse.get_pressed()
+        mousePosition = pygame.mouse.get_pos()
+        tile = get_cur_tile(mousePosition)
+        if mousePresses[0]:
+            if tile != None:
+                tile.enabled = False
+        elif mousePresses[1]:
+            if tile != None:
+                tile.enabled = True
+                tile.color = (0,0,255)
+        elif mousePresses[2]:
+            if tile != None:
+                tile.enabled = True
+                tile.color = (255,255,255)
     window.fill((200,200,200))
+    for tile in tile_list:
+        tile.draw()
     pygame.display.update()
 
 pygame.quit()
-quit()
